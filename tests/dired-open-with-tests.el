637ebf89c65fb76734fa23f35a07dd891475228b
Jakub Kadlcik <frostyx@email.cz>
2024-09-20 10:51:14 +0200

    Don't error with unknown MIME type for directories
    Fix #7

--- tests/dired-open-with-tests.el
+++ tests/dired-open-with-tests.el
@@ -1,3 +1,7 @@
 ;;; dired-open-with-tests.el --- Tests for dired-open-with -*- lexical-binding: t; -*-
 
 (require 'ert)
+(require 'dired-open-with)
+
+(ert-deftest dired-open-with--mimetype ()
+  (should (equal (dired-open-with--mimetype "/foo/bar/baz.png") "image/png")))
